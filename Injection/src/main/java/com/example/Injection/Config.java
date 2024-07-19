91264a61aa659c9799087a7d71948182d74b8d4c
Cosmin <109922270+PantiruCosminNarcis@users.noreply.github.com>
2024-07-19 13:41:32 +0300

    Add files via upload

---
+++ Injection/src/main/java/com/example/Injection/Config.java
@@ -0,0 +1,22 @@
+package com.example.Injection;
+
+import com.example.Injection.domain.Engine;
+import com.example.Injection.domain.Transmission;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.Injection")
+public class Config {
+
+    @Bean
+    public Engine engine() {
+        return new Engine("v8", 5);
+    }
+
+    @Bean
+    public Transmission transmission() {
+        return new Transmission("sliding");
+    }
+}
---
+++ Injection/src/main/java/com/example/Injection/SpringRunner.java
@@ -0,0 +1,29 @@
+package com.example.Injection;
+import com.example.Injection.domain.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringRunner {
+    public static void main(String[] args) {
+        Car toyota = getCarFromXml();
+
+        System.out.println(toyota);
+
+        toyota = getCarFromJavaConfig();
+
+        System.out.println(toyota);
+    }
+
+    private static Car getCarFromJavaConfig() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+        return context.getBean(Car.class);
+    }
+
+    private static Car getCarFromXml() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Injection.xml");
+
+        return context.getBean(Car.class);
+    }
+}
